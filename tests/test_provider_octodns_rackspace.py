1b173388e2a25e51f04a45a57e44af270dd5315a
Ross McFarland <rwmcfa1@gmail.com>
2022-01-14 13:20:39 -0800

    Import skeleton

---
+++ tests/test_provider_octodns_rackspace.py
@@ -0,0 +1,15 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_rackspace import RackspaceProvider
+
+
+class TestRackspaceProvider(TestCase):
+
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        RackspaceProvider
